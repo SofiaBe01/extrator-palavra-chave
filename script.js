0a6c0f9e2bde45c4029ad7ae7a942a55ed764b98
SofiaBe01 <sofia.beatriz.silva@escola.pr.gov.br>
2025-07-28 11:34:31 -0300

    a

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
+    const palavras = texto.split(" ");
     const campoResultado = document.querySelector("#resultado-palavrachave");
+    campoResultado.textContent = palavras.join(",");
 }
